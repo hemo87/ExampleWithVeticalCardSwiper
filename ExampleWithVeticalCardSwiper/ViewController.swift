5c318c9abcb626942251fb96159c26ab4ba04286
Marc Heminsley <marc.heminsley@me.com>
2019-04-08 11:48:40 +0100

    Initial Commit

---
+++ ExampleWithVeticalCardSwiper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleWithVeticalCardSwiper
+//
+//  Created by Marc Heminsley on 08/04/2019.
+//  Copyright © 2019 MarcHeminsley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
